d751686ec22c44916241b058972f56f889305d50
Lucas da Silva <sklucassilva@gmail.com>
2025-02-27 20:26:17 -0300

    feat: Adicionar suporte à configuração do banco de dados a partir de um arquivo XML

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,22 @@ import logger from './config/winston';
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use('/api', queryRoutes);
 
-AppDataSource.initialize()
-  .then(async () => {
+async function startServer() {
+  try {
+    const dataSource = await AppDataSource; // Aguarda a resolução da Promise
+    await dataSource.initialize();
     logger.info('Conectado com sucesso ao banco de dados');
-  })
-  .catch(() => logger.error('Erro ao conectar ao banco de dados'));
 
-app.listen(3333, () => {
-  logger.info('Servidor rodando na porta 3333');
-});
+    app.listen(3333, () => {
+      logger.info('Servidor rodando na porta 3333');
+    });
+  } catch (error) {
+    logger.error('Erro ao conectar ao banco de dados', error);
+    process.exit(1); // Encerra a aplicação se o banco não conectar
+  }
+}
+
+startServer();
