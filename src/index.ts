1b420b16f04382d72f62b97f7e6d5be26fba70e6
Lucas da Silva <sklucassilva@gmail.com>
2025-02-28 13:34:15 -0300

    refactor: Simplificar a inicialização do servidor e a conexão com o banco de dados

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,23 @@ import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
 import { AppDataSource } from './database/data-source';
-import queryRoutes from './routes/query.routes';
 import logger from './config/winston';
+import queryRoutes from './routes/query.routes';
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
+
 app.use('/api', queryRoutes);
 
-async function startServer() {
-  try {
-    const dataSource = await AppDataSource; // Aguarda a resolução da Promise
-    await dataSource.initialize();
+AppDataSource.initialize()
+  .then(async () => {
     logger.info('Conectado com sucesso ao banco de dados');
+  })
+  .catch(() => logger.error('Erro ao conectar ao banco de dados'));
 
-    app.listen(3333, () => {
-      logger.info('Servidor rodando na porta 3333');
-    });
-  } catch (error) {
-    logger.error('Erro ao conectar ao banco de dados', error);
-    process.exit(1); // Encerra a aplicação se o banco não conectar
-  }
-}
-
-startServer();
+app.listen(3333, () => {
+  logger.info('Servidor rodando na porta 3333');
+});
